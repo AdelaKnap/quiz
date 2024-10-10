11235adb60ec973a05e8a0db804cca8e9beb0a85
Adela Knap <adkn2300@student.miun.se>
2024-10-10 10:05:40 +0200

    first commit

---
+++ Dog.cs
@@ -0,0 +1,11 @@
+// Klass för Dog med frågan och svaret
+
+namespace quiz
+{
+    public class Dog
+    {
+        public string? Question { get; set; }
+
+        public string? Breed { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿/* DT071G Projektarbete. En konsolapplikation med ett quiz med hundfrågor. Av Adela Knap */
+
+using static System.Console;  // För att slippa skriva Console framför Write/WriteLine
+
+namespace quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+        }
+
+    }
+}
